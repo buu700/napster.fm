46acef52e2dbf4ecc2a90bb79fdda1197a77e34d
Ryan Lester <hacker@linux.com>
2012-12-23 02:35:37 -0800

    Angular test

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,10 @@
-var PhoneListCtrl	= function ($scope) {
-	$scope.phones = [
+window.phones = [
 		{"name": "Nexus S", "snippet": "Fast just got faster with Nexus S."},
 		{"name": "Motorola XOOM™ with Wi-Fi", "snippet": "The Next, Next Generation tablet."},
 		{"name": "MOTOROLA XOOM™", "snippet": "The Next, Next Generation tablet."}
 	];
+
+var PhoneListCtrl	= function ($scope) {
+	$scope.phones = window.phones;
 	new Firebase('https://napsterfm.firebaseio.com/dude').on('value', function (value) { $scope.phones[1].name = value.val(); $scope.$apply(); })
 };
