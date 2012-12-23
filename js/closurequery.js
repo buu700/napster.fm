694f9421a777267b6dcde6c10a2ac4802acd35db
Ryan Lester <hacker@linux.com>
2012-12-23 00:41:44 -0800

    Extended ClosureQuery to behave a little more like jQuery (syntactic sugar for creating DOM elements)

--- js/closurequery.js
+++ js/closurequery.js
@@ -3,12 +3,18 @@ var domHelpers	= Object.keys(goog.dom).filter(function (key) { return !key.has('
 var $	= function (selector) {
 	var elements	= goog.dom.query(selector);
 
+	if (elements.length < 1 && new DOMParser().parseFromString(selector, 'text/xml').getElementsByTagName('parsererror').length < 1) {
+		var tempElem		= goog.dom.createElement('div');
+		tempElem.innerHTML	= selector;
+		elements[0]			= tempElem.firstChild;
+	}
+
 	domHelpers.forEach(function (key) {
 		for( var i = 0 ; i < elements.length ; ++i ) {
 			elements[i][key]	= goog.dom[key].fill(elements[i]);
 		}
 
-		elements[key]	= elements[0][key];
+		elements[key]	= elements[0] && elements[0][key];
 	});
 
 	return elements;
