9b332d9eef6c2a1698b647517bb3fcebbf4f60c5
Ryan Lester <hacker@linux.com>
2012-12-22 23:03:38 -0800

    Got bored and hacked Closure DOM APIs into jQuery lookalike (I'm going to Hell for this)

---
+++ js/closurequery.js
@@ -0,0 +1,32 @@
+goog.require('goog.dom');
+goog.require('goog.dom.query');
+
+
+Function.prototype.partial	= function () {
+	var fn		= this;
+	var args	= Array.prototype.slice.call(arguments);
+	return function () {
+		var arg	= 0;
+		for ( var i = 0; i < args.length && arg < arguments.length; ++i ) {
+			if ( args[i] === undefined ) {
+				args[i]	= arguments[arg++];
+			}
+		}
+		return fn.apply(this, args);
+	};
+};
+
+
+var domHelpers	= Object.keys(goog.dom).filter(function (key) { return !key.has('_') && typeof goog.dom[key] === 'function'; });
+
+var $	= function (selector) {
+	var elements	= goog.dom.query(selector);
+
+	domHelpers.forEach(function (key) {
+		for( var i = 0 ; i < elements.length ; ++i ) {
+			elements[i][key]	= goog.dom[key].partial(elements[i]);
+		}
+	});
+
+	return elements;
+};
