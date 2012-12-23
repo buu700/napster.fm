438535714a022e0c49b2f8240708fafbeaee56bb
Ryan Lester <hacker@linux.com>
2012-12-23 01:48:56 -0800

    Angular test

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var PhoneListCtrl	= function ($scope) {
+function PhoneListCtrl ($scope) {
 	$scope.phones = [
 		{"name": "Nexus S", "snippet": "Fast just got faster with Nexus S."},
 		{"name": "Motorola XOOM™ with Wi-Fi", "snippet": "The Next, Next Generation tablet."},
