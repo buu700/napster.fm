60caf7ac259d18f81f3ffe7062bf787cb196b06f
Ryan Lester <hacker@linux.com>
2012-12-22 23:50:21 -0800

    ClosureQuery: swapped custom Function.prototype.partial for .fill from Sugar API

--- js/closurequery.js
+++ js/closurequery.js
@@ -1,18 +1,3 @@
-Function.prototype.partial	= function () {
-	var fn		= this;
-	var args	= Array.prototype.slice.call(arguments);
-	return function () {
-		var arg	= 0;
-		for ( var i = 0; i < args.length && arg < arguments.length; ++i ) {
-			if ( args[i] === undefined ) {
-				args[i]	= arguments[arg++];
-			}
-		}
-		return fn.apply(this, args);
-	};
-};
-
-
 var domHelpers	= Object.keys(goog.dom).filter(function (key) { return !key.has('_') && typeof goog.dom[key] === 'function'; });
 
 var $	= function (selector) {
@@ -20,7 +5,7 @@ var $	= function (selector) {
 
 	domHelpers.forEach(function (key) {
 		for( var i = 0 ; i < elements.length ; ++i ) {
-			elements[i][key]	= goog.dom[key].partial(elements[i]);
+			elements[i][key]	= goog.dom[key].fill(elements[i]);
 		}
 
 		elements[key]	= elements[0][key];
