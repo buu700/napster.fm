1f59b98a48b063ea86b2def0983045f29d81d3c3
Ryan Lester <hacker@linux.com>
2012-12-31 04:28:51 -0800

    Basic YouTube API integration

---
+++ js/stream.js
@@ -0,0 +1,64 @@
+var stream	= new function () {
+
+
+
+
+/**
+* @namespace Player
+*/
+var self	= this;
+
+
+
+
+/**
+* @field
+* @property {Node}
+*/
+var player;
+
+
+
+
+/**
+* @function
+* @property {void} Initialises this namespace
+*/
+var init;
+
+/**
+* @function
+* @property {void} Loads track in player
+* @param {string} track
+*/
+var loadTrack;
+
+
+
+
+self.init	= function () {
+	window.onYouTubePlayerReady = function () { stream.player = $('#streamPlayer')[0]; };
+
+	swfobject.embedSWF(
+		'http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer&version=3',
+		'stream',
+		'200',
+		'200',
+		'8',
+		null,
+		null,
+		{allowScriptAccess: 'always'},
+		{id: 'streamPlayer'}
+	);
+};
+
+
+self.loadTrack	= function (track) {
+	self.player.loadVideoById(track);
+	datastore.track(track).lastPlayed.set(authentication.userid);
+};
+
+
+
+
+};
