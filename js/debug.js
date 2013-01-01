953383284f4b377ea57d2b361cb999ed6772ebf3
Ryan Lester <hacker@linux.com>
2013-01-01 03:58:15 -0800

    debug class

---
+++ js/debug.js
@@ -0,0 +1,31 @@
+var debug	= new function () {
+
+
+
+
+/**
+* @namespace Debug
+*/
+var self	= this;
+
+
+
+
+/**
+* @function
+* @property {void} Logs message
+* @param {string} message
+*/
+var log;
+
+
+
+
+self.log	= function (message) {
+	console.log(message);
+};
+
+
+
+
+};
