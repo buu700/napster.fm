0a87c3ab28ce428f3551a66345a697855382eedf
Ryan Lester <hacker@linux.com>
2013-01-04 00:16:49 -0800

    debug.logVal

--- js/debug.js
+++ js/debug.js
@@ -18,11 +18,22 @@ var self	= this;
 */
 var log;
 
+/**
+* @function
+* @property {void} Logs message
+* @param {string} message
+*/
+var logVal;
+
 
 
 
 self.log	= function (message) {
 	console.log(message);
+};
+
+self.logVal	= function (message) {
+	console.log(message.val());
 };
 
 
