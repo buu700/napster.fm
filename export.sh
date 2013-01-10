6e85f9138231702b1de26729433857c222e3597d
Ryan Lester <hacker@linux.com>
2013-01-10 01:18:56 -0800

    Exports fix

--- export.sh
+++ export.sh
@@ -5,6 +5,9 @@ cd js
 
 echo -e "goog.provide('exports');\n\n" > exports.js
 
+
+echo "goog.exportSymbol('Napster', Napster);" >> exports.js
+
 for namespace in ${@} ; do
 	grep '^var .*;' "${namespace}.js" | grep -v '=' | while read var ; do var="${namespace}.${var:4:${#var}-5}" ; var="${var%;}" ; echo "goog.exportSymbol('${var}', ${var});" >> exports.js ; done
 done
