d68737c4ed4698906c1a1cb361f49eb4676e19db
Ryan Lester <hacker@linux.com>
2013-01-10 02:47:44 -0800

    Add goog.require statements for Napster classes

--- js/require.js
+++ js/require.js
@@ -12,3 +12,14 @@ goog.require('goog.object');
 goog.require('goog.ui.Component');
 goog.require('goog.ui.Slider');
 goog.require('goog.ui.TableSorter');
+
+
+goog.require('authentication');
+goog.require('closurequery');
+goog.require('controllers');
+goog.require('datastore');
+goog.require('debug');
+goog.require('init');
+goog.require('services');
+goog.require('stream');
+goog.require('ui');
