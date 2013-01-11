237695114b6d9cd0f81ed48562e6f4ca7cb6fef5
Ryan Lester <hacker@linux.com>
2013-01-10 21:07:04 -0800

    deployment

--- export.sh
+++ export.sh
@@ -9,6 +9,6 @@ echo -e "goog.provide('exports');\n\n" > exports.js
 echo "goog.exportSymbol('Napster', Napster);" >> exports.js
 
 for namespace in ${@} ; do
-	"goog.exportSymbol('${namespace}', ${namespace});" >> exports.js
+	echo "goog.exportSymbol('${namespace}', ${namespace});" >> exports.js
 	grep '^var .*;' "${namespace}.js" | grep -v '=' | while read var ; do var="${namespace}.${var:4:${#var}-5}" ; var="${var%;}" ; echo "goog.exportSymbol('${var}', ${var});" >> exports.js ; done
 done
