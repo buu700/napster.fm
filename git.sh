a9314414aa5d6bbb556dfceaa9afddac7f613770
Ryan Lester <hacker@linux.com>
2013-01-11 00:19:51 -0800

    deployment

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-while read comment ; do chmod 777 -R . ; git add . ; git commit -a -m "${comment}" ; git push ; echo -e "Line count: `cat schema.json *.html css/*.css js/*.js | wc -l`\n\n\n" ; ./deploy.sh ; done
+while read comment ; do chmod 777 -R . ; git add . ; git commit -a -m "${comment}" ; git push ; echo -e "Line count: `ls schema.json *.html css/*.css js/*.js | grep -v externs.js | xargs cat | wc -l`\n\n\n" ; ./deploy.sh ; done
