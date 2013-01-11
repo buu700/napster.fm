96e6b1f50313bdaec6fa2753dd7278fcd678a92c
Ryan Lester <hacker@linux.com>
2013-01-10 21:20:13 -0800

    Exports goog.require

--- export.sh
+++ export.sh
@@ -6,9 +6,10 @@ cd js
 echo -e "goog.provide('exports');\n\n" > exports.js
 
 
-echo "window['Napster'] = Napster;" >> exports.js
-
 for namespace in ${@} ; do
+	echo "goog.require('${namespace}');" >> exports.js
 	echo "window['${namespace}'] = ${namespace};" >> exports.js
 	grep '^var .*;' "${namespace}.js" | grep -v '=' | while read var ; do var="${var:4:${#var}-5}" ; var="${var%;}" ; echo "window['${namespace}']['${var}'] = ${namespace}.${var};" >> exports.js ; done
 done
+
+echo "window['Napster'] = Napster;" >> exports.js
