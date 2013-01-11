7b4283572a366578f549344c9ca674f11608a4e1
Ryan Lester <hacker@linux.com>
2013-01-10 21:32:07 -0800

    Circular dependency fix

--- js/require.js
+++ js/require.js
@@ -21,7 +21,6 @@ goog.require('closurequery');
 goog.require('controllers');
 goog.require('datastore');
 goog.require('debug');
-goog.require('init');
 goog.require('services');
 goog.require('stream');
 goog.require('ui');
