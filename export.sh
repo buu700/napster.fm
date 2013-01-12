40d62a94274b81dc88b44e7bd79e62b08a699987
Ryan Lester <hacker@linux.com>
2013-01-12 15:09:10 -0800

    deployment

--- export.sh
+++ export.sh
@@ -8,6 +8,9 @@ echo -e "goog.provide('exports');\n\n" > exports.js
 
 for namespace in ${@} ; do
 	echo "goog.require('${namespace}');" >> exports.js
+
+
+for namespace in ${@} ; do
 	echo "window['${namespace}'] = ${namespace};" >> exports.js
 	grep '^var .*;' "${namespace}.js" | grep -v '=' | while read var ; do var="${var:4:${#var}-5}" ; var="${var%;}" ; echo "window['${namespace}']['${var}'] = ${namespace}.${var};" >> exports.js ; done
 done
