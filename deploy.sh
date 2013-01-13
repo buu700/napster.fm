54ce60cacc4f6c69512def5f147aaee5f593e1ee
Ryan Lester <hacker@linux.com>
2013-01-12 16:10:52 -0800

    deployment

--- deploy.sh
+++ deploy.sh
@@ -31,11 +31,6 @@ cd ..
 
 
 ./export.sh "${namespaces[@]}"
-
-for file in `ls js/*.js` ; do
-	sed -i "s/\/\* require \*\//`cat js/require* | grep goog.require | tr '\n' ' ' | sed 's/ /\\\\n/g'`/" "${file}"
-done
-
 js/closure-library/closure/bin/build/closurebuilder.py --root=js $namespaceArgs -n require.goog -n require.napster -n exports -n init --output_mode=compiled --compiler_jar=compiler.jar --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" --compiler_flags="--externs=js/externs.js" --output_file=js/napster.js # .tmp
 # java -jar yuicompressor.jar --type js -o js/napster.js js/napster.js.tmp
 # rm js/napster.js.tmp
