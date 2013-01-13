1ac9d74c8dac4b992a69a016441bfd442f264899
Ryan Lester <hacker@linux.com>
2013-01-13 00:23:10 -0800

    deployment

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,27 @@
 goog.provide('ui');
 
-/* require */
+goog.require('goog.async.ConditionalDelay');
+goog.require('goog.async.Deferred');
+goog.require('goog.async.DeferredList');
+goog.require('goog.crypt.Hmac');
+goog.require('goog.crypt.Sha256');
+goog.require('goog.dom');
+goog.require('goog.dom.classes');
+goog.require('goog.dom.query');
+goog.require('goog.net.cookies');
+goog.require('goog.net.Jsonp');
+goog.require('goog.object');
+goog.require('goog.ui.Component');
+goog.require('goog.ui.Slider');
+goog.require('goog.ui.TableSorter');
+
+goog.require('authentication');
+goog.require('closurequery');
+goog.require('controllers');
+goog.require('datastore');
+goog.require('debug');
+goog.require('services');
+goog.require('stream');
 
 
 var ui	= new function () {
