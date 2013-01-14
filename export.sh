8dca339e31de7d030aeaf1a3752651c6e1235835
Ryan Lester <hacker@linux.com>
2013-01-13 17:34:54 -0800

    deployment

--- export.sh
+++ export.sh
@@ -18,20 +18,14 @@ echo -e 'window["exports"] = {' >> exports.js
 
 
 for namespace in ${@} ; do
-	echo "\"${namespace}\": ${namespace}," >> exports.js
-	grep '^var .*;' "${namespace}.js" | grep -v '=' | while read var ; do var="${var:4:${#var}-5}" ; var="${var%;}" ; echo "\"${namespace}.${var}\": ${namespace}.${var}," >> exports.js ; done
+	cat ../*.html | grep -oP "${namespace}\..*?['\",};]" | while read member ; do
+		member="${member:0:${#member}-1}"
+		echo "\"${member}\": ${member}," >> exports.js
+	done
 done
 
 
-echo "\"datastore.data.group\": datastore.data.group," >> exports.js
-echo "\"datastore.data.lastPlayed\": datastore.data.lastPlayed," >> exports.js
-echo "\"datastore.data.lastPlayed.processed\": datastore.data.lastPlayed.processed," >> exports.js
-echo "\"datastore.data.track\": datastore.data.track," >> exports.js
-echo "\"datastore.data.user\": datastore.data.user," >> exports.js
-echo "\"datastore.data.user.current\": datastore.data.user.current" >> exports.js
-
-
-echo -e '};' >> exports.js
+echo -e '"END": "END" };' >> exports.js
 
 
 echo -e '/** @preserve EXPORT END */' >> exports.js
