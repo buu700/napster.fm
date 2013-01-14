756557b1d91082462d1880b4fdcee63787c9761a
Ryan Lester <hacker@linux.com>
2013-01-13 19:26:18 -0800

    deployment

--- js/exports.js
+++ js/exports.js
@@ -10,19 +10,3 @@ goog.require('require');
 goog.require('services');
 goog.require('stream');
 goog.require('ui');
-window["exports"] = {
-"authentication.username": authentication.username,
-"datastore.data.user.current.nowPlayingProcessed.title": datastore.data.user.current.nowPlayingProcessed.title,
-"datastore.data.user.current.nowPlayingProcessed.artist": datastore.data.user.current.nowPlayingProcessed.artist,
-"datastore.data.user.current.nowPlayingProcessed.length": datastore.data.user.current.nowPlayingProcessed.length,
-"datastore.data.user.current.isOnline": datastore.data.user.current.isOnline,
-"datastore.data.user.current.library.processed": datastore.data.user.current.library.processed,
-"stream.play": stream.play,
-"stream.isPlaying": stream.isPlaying,
-"stream.newTimeProcessed": stream.newTimeProcessed,
-"stream.loadTrack": stream.loadTrack,
-"ui.playButtonClass": ui.playButtonClass,
-"ui.slider.onclick": ui.slider.onclick,
-"ui.pageLoadingClass": ui.pageLoadingClass,
-"ui.pageLoadingClass": ui.pageLoadingClass,
-"EXPORT.END": "EXPORT.END" };
