4e2da9732eeb7f0aaf6b6b5ff9fa377e86518f32
Ryan Lester <hacker@linux.com>
2013-01-13 17:44:23 -0800

    re-added json extraction/removal/parsing + html minified member injection; workaround for @preserve comment merging

--- deploy.sh
+++ deploy.sh
@@ -34,6 +34,22 @@ echo -e "goog.provide('init');\n\ngoog.require('exports');\n\n`cat js/init.js`" 
 ./export.sh "${namespaces[@]}"
 js/closure-library/closure/bin/build/closurebuilder.py --root=js $namespaceArgs -n exports -n init --output_mode=compiled --compiler_jar=compiler.jar --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" --compiler_flags="--externs=js/externs.js" --output_file=js/napster.js
 
+# Inject minified member names into html
+function jsonify { json="`echo "${1}" | sed 's/ //g' | sed 's/\":/":"/g' | sed 's/,"/","/g'`" && echo "${json:0:${#json}-1}\"}" | python -mjson.tool; }
+function jsonval { jsonify "${1}" | grep "${2}" | head -n1 | sed -E 's/.*".*": "(.*)".*/\1/'; }
+function jsonkeys { jsonify "${1}" | grep ':' | sed -E 's/.*"(.*)":.*/\1/g' | tr '\n' ' '; }
+
+exports="`cat js/napster.js | sed -E 's/.*EXPORT .*=(.*});.*/\1/'`"
+cat js/napster.js | sed -E 's/\/\*.*EXPORT.*};//' > js/napster.js.tmp
+mv js/napster.js.tmp js/napster.js
+
+for key in "`jsonkeys "${exports}"`" ; do
+	value="`jsonval "${exports}" "${key}"`"
+	for file in "`ls *.html`" ; do
+		sed -i "s/${key}/${value}/g" "${file}"
+	done
+done
+
 # java -jar yuicompressor.jar --nomunge --type js -o js/napster.js.tmp js/napster.js
 # mv js/napster.js.tmp js/napster.js
 
