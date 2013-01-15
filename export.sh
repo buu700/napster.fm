fdbbd599def6a5f05c29a1a507093fca12c52129
Ryan Lester <hacker@linux.com>
2013-01-14 23:13:54 -0800

    fixed silly bug and removed debuging output from regexInPlace

--- export.sh
+++ export.sh
@@ -14,7 +14,7 @@ for namespace in "${@}" ; do
 done
 
 
-function regexInPlace { cat "${2}" | perl -pe "${1}" > ".${2}.tmp" && mv ".${2}.tmp" "${2}" && echo "${2}"; }
+function regexInPlace { cat "${2}" | perl -pe "${1}" > ".${2}.tmp" && mv ".${2}.tmp" "${2}"; }
 level="\['.*'\]"
 
 function exportMembers {
@@ -22,7 +22,9 @@ function exportMembers {
 	# Just making the modification an arbitrarily high number of levels deep for now because I don't care enough to find a more elegant solution
 	for i in {0..100} ; do
 		levels="`yes "${level}" | head -n${i} | tr '\n' ' ' | sed 's/ //g'`" # $level * $i
-		regexInPlace "s/(${namespace}${levels})\.(.*?)([^A-Za-z0-9$_])/\1\['\2'\]\3/g" *.js
+		for file in "`ls *.js`" ; do
+			regexInPlace "s/(${namespace}${levels})\.(.*?)([^A-Za-z0-9$_])/\1\['\2'\]\3/g" "${file}"
+		done
 	done
 }
 
