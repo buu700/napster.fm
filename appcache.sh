2719b55786ba2b6284130b7d9c6b030bb3d9b10c
Ryan Lester <hacker@linux.com>
2013-02-09 11:26:13 -0800

    appcache.sh fix

--- appcache.sh
+++ appcache.sh
@@ -8,5 +8,5 @@ echo -e "# `date +%s`\n\n"
 echo -e 'NETWORK:\n*\n'
 
 echo 'CACHE:'
-grep "='//.*'" index.html | perl -pe "s/.*'(\/\/.*)'.*/http:\1/g"
+cat index.html | tr ' ' '\n' | grep "='//.*'" | perl -pe "s/.*'(\/\/.*)'.*/http:\1/g"
 find . -type f | grep -v closure | grep -v .git | while read path ; do echo "${path:2}" ; done
