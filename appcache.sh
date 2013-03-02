9ab288ecb8aaf9caf54c6c5de1a9b7de32a23397
Ryan Lester <hacker@linux.com>
2013-03-02 13:05:57 -0800

    deployment

--- appcache.sh
+++ appcache.sh
@@ -9,4 +9,5 @@ echo -e 'NETWORK:\n*\n'
 
 echo 'CACHE:'
 cat index.html | tr ' ' '\n' | grep "='//.*'" | perl -pe "s/.*'(\/\/.*)'.*/http:\1/g"
-find . -type f -name '*.js' -o -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo "${path:2}" ; done
+find . -type f -o -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo "${path:2}" ; done
+echo 'js/napster.js'
