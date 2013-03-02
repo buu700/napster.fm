0cd4c976737b3f4aef845a46ef46002ec8ceaab4
Ryan Lester <hacker@linux.com>
2013-03-02 13:22:19 -0800

    export fix

--- export.sh
+++ export.sh
@@ -10,5 +10,9 @@ echo -e "goog.require('napster.require');" >> exports.js
 
 for namespace in "${@}" ; do
 	echo "goog.require('napster.${namespace}');" >> exports.js
+
+	# Expose $ instead of closurequery for closurequery
+	test "${namespace}" == 'closurequery' && namespace='$'
+	
 	echo -e "/** @expose */\n${namespace};" >> exports.js
 done
