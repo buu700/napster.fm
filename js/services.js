08cd0752eed4e7ea07cd8aef9c0fedba1346f3f0
Ryan Lester <hacker@linux.com>
2013-03-02 21:24:52 -0800

    deployment

--- js/services.js
+++ js/services.js
@@ -1,4 +1,20 @@
-goog.provide('napster.services');
+goog.require('goog.async.ConditionalDelay');
+goog.require('goog.async.Deferred');
+goog.require('goog.async.DeferredList');
+goog.require('goog.crypt.Hmac');
+goog.require('goog.crypt.Sha256');
+goog.require('goog.dom');
+goog.require('goog.dom.classes');
+goog.require('goog.dom.query');
+goog.require('goog.events');
+goog.require('goog.events.KeyCodes');
+goog.require('goog.events.KeyHandler');
+goog.require('goog.net.cookies');
+goog.require('goog.net.Jsonp');
+goog.require('goog.object');
+goog.require('goog.ui.Component');
+goog.require('goog.ui.Slider');
+goog.require('goog.ui.TableSorter');goog.provide('napster.services');
 
 
 var services	= new function () {
