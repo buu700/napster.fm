0c844612e85901d5f143eb60f5a291e863558747
Ryan Lester <hacker@linux.com>
2013-03-11 22:14:16 -0700

    deployment

--- js/chat.js
+++ js/chat.js
@@ -161,7 +161,7 @@ self.removeFromGroup	= function (groupid) {
 
 	/* Switch to arbitrary remaining group after removal when applicable */
 	var newGroup	= goog.object.getKeys(datastore.data.user.current.groups).remove(groupid)[0];
-	newGroup && self.switchToGroup(newGroup);
+	newGroup ? self.switchToGroup(newGroup) : ui.update();
 };
 
 self.switchToGroup	= function (groupid) {
