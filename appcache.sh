98256d44e17b5fc23c63be28c6d60ab2aaaf66a3
Ryan Lester <hacker@linux.com>
2013-03-17 18:56:57 -0700

    appcache fix

--- appcache.sh
+++ appcache.sh
@@ -8,6 +8,6 @@ echo -e "# `date +%s`\n\n"
 echo -e 'NETWORK:\n*\n'
 
 echo 'CACHE:'
-cat index.html | tr ' ' '\n' | grep ".*='//.*'" | grep -v '<a' | grep -v firebase | grep -v youtube | perl -pe "s/.*'(\/\/.*)'.*/http:\1/g"
+cat index.html | grep ".*='//.*'" | grep -v '<a' | grep -v firebase | grep -v youtube | perl -pe "s/.*'(\/\/.*)'.*/http:\1/g"
 find . -type f -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo "${path:2}" ; done
 echo 'js/napster.js'
