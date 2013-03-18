a3aa39f0c4f4d9edb7ff1bee992de73095b13f21
Ryan Lester <hacker@linux.com>
2013-03-17 19:18:27 -0700

    deployment

--- appcache.sh
+++ appcache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-comment="# `date +%s`\n"
+comment="# `date +%s`"
 
 echo -e	'CACHE MANIFEST\n'
 
@@ -9,5 +9,5 @@ echo -e 'NETWORK:\n*\n'
 
 echo 'CACHE:'
 cat index.html | grep ".*='//.*'" | grep -v '<a' | grep -v firebase | grep -v youtube | perl -pe "s/.*'(\/\/.*)'.*/http:\1 ${comment}/g"
-find . -type f -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo -e "${path:2} ${comment}" ; done
-echo -e "js/napster.js ${comment}"
+find . -type f -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo "${path:2} ${comment}" ; done
+echo "js/napster.js ${comment}"
