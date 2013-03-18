fe258d4fdbfc1fe158dc901ff1faf90034a6f413
Ryan Lester <hacker@linux.com>
2013-03-17 19:14:02 -0700

    possible appcache fix

--- appcache.sh
+++ appcache.sh
@@ -8,6 +8,6 @@ echo -e	'CACHE MANIFEST\n'
 echo -e 'NETWORK:\n*\n'
 
 echo 'CACHE:'
-cat index.html | grep ".*='//.*'" | grep -v '<a' | grep -v firebase | grep -v youtube | perl -pe "s/.*'(\/\/.*)'.*/${comment}http:\1/g"
-find . -type f -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo -e "${comment}${path:2}" ; done
-echo -e "${comment}js/napster.js"
+cat index.html | grep ".*='//.*'" | grep -v '<a' | grep -v firebase | grep -v youtube | perl -pe "s/.*'(\/\/.*)'.*/http:\1 ${comment}/g"
+find . -type f -name '*.css' -o -name '*.html' -o -name '*.png' | grep -v closure | grep -v .git | while read path ; do echo -e "${path:2} ${comment}" ; done
+echo -e "js/napster.js ${comment}"
