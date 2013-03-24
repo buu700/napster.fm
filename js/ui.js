f9128c29297af7b6ff0358bb4b1f078b557c4b6d
Ryan Lester <hacker@linux.com>
2013-03-24 15:32:40 -0700

    deployment

--- js/ui.js
+++ js/ui.js
@@ -148,7 +148,7 @@ self.init	= function () {
 			}
 		};
 
-		setActive('.navbar a', 'href', function ($elem) { return $elem.parentNode; });
+		setActive('#navbar a', 'href', function ($elem) { return $elem.parentNode; });
 		setActive('[hash-location]', 'hash-location');
 	};
 
@@ -189,7 +189,6 @@ self.init	= function () {
 
 	/* Open all links to external resources in new tabs */
 	$('a[href^="//"]').each(function ($elem) { $elem.setProperties({'target': '_blank'}); });
-
 
 
 	/*** onenterpress attribute handler ***/
@@ -234,7 +233,7 @@ self.update	= function () {
 	self.tempUserClass			= self.tempUserClass || (authentication.username && authentication.username.startsWith('temporary-account-') ? 'temp-user' : '');
 
 	goog.object.forEach(datastore.data.user.current.library, function (o) {
-		o.nowPlayingClass	= o.id == stream.currentTrack && 'now-playing';
+		o.nowPlayingClass	= o.id == stream.currentTrack ? 'now-playing' : '';
 	});
 
 	var $following	= $('#following')[0];
