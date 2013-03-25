7e5ac779afc3aadbd8a0a9dce9697f44421141c5
Ryan Lester <hacker@linux.com>
2013-03-24 18:30:19 -0700

    deployment

--- js/init.js
+++ js/init.js
@@ -25,3 +25,11 @@ datastore.init();
 ui.init();
 stream.init();
 chat.init();
+
+
+/* Fallback in case Firebase fails to load something */
+window.setTimeout(function () {
+	if (!ui.readyClass) {
+		document.location.reload(true);
+	}
+}, 60000);
