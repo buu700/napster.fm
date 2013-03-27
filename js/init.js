f0071823514b7a201d9962ee5fe367407b9dd62a
Ryan Lester <hacker@linux.com>
2013-03-26 17:51:47 -0700

    Google Analytics

--- js/init.js
+++ js/init.js
@@ -12,3 +12,15 @@ window.setTimeout(function () {
 		document.location.reload(true);
 	}
 }, 60000);
+
+
+/* Google Analytics */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-25158938-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
